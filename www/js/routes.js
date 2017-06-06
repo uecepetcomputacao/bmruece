82449d0b98421b7a8bc82ee82f154770a662f2d0
Aurelio <aurelioararipe@gmail.com>
2017-06-06 02:30:17 -0300

    primeiras rotas. Funcionalidade: biblioteca, mapas e ru

---
+++ www/js/routes.js
@@ -0,0 +1,32 @@
+angular.module('starter')
+.config(function($stateProvider, $urlRouterProvider){
+
+	$urlRouterProvider.otherwise('/listagemApps');
+
+	$stateProvider
+
+	.state('listagemApps', {
+		url: '/listagemApps',
+		templateUrl: 'templates/listagemApps.html',
+		controller: 'ListagemController'
+	})
+
+	.state('biblioteca', {
+		url: '/biblioteca',
+		templateUrl: 'templates/biblioteca.html',
+		controller: 'BibliotecaController'
+	})
+
+	.state('restauranteuniversitario', {
+		url: '/restauranteuniversitario',
+		templateUrl: 'templates/restauranteuniversitario.html',
+		controller: 'RestauranteUniversitarioController'
+	})
+
+	.state('mapas', {
+		url: '/mapas',
+		templateUrl: 'templates/mapas.html',
+		controller: 'MapasController'
+	})
+
+});
